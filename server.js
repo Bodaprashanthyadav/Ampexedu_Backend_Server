34ad1be6e065f5d9fb68fa5a0c549f96def720c5
Boda prashanth yadav <b.prash2001@gmail.com>
2025-03-18 03:34:09 +0530

    server file update

--- server.js
+++ server.js
@@ -1,41 +1,40 @@
 const express = require('express')
 const cors = require('cors')
 
-const dotenv= require('dotenv')
+const dotenv = require('dotenv')
 dotenv.config();
 // mongoDB cnnection
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGO_URI)
     .then((res) => { console.log('MongoDB successfully connected ') })
     .catch((err) => { console.log('MongoDB Failed to  connect' + err) })
 
 const bodyParser = require('body-parser')
 var app = express()
 var Users = require('./Model/UsersDetails.model')
-var PORT = 5600
+
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+var PORT = process.env.PORT || 5800;
 
-// app.use(express.static(__dirname))
+
 
 
 
 // routes
 
-
-
 app.post('/usersdetails', (req, res) => {
     console.log(req.body)
     var user = new Users(req.body)
     user.save().then(data => {
-        res.status(200).json({msg:'Thank You! We Will Get Back to You Soon!'});
+        res.status(200).json({ msg: 'Thank You! We Will Get Back to You Soon!' });
 
     })
-    .catch(err => {
-        res.status(500).json({msg:'Error in submiting Data'})
-   })
+        .catch(err => {
+            res.status(500).json({ msg: 'Error in submiting Data' })
+        })
 
 })
 
